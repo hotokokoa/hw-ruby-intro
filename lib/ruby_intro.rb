101560cf78589a2e6911f92c9b30ac2eea2ce529
Mio <9just.k.on@gmail.com>
2016-05-05 03:53:48 +0000

    p2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -45,45 +45,38 @@ end
 # Part 2
 
 def hello(name)
-
-  s = "Hello," + name
+  s = "Hello, " + name
   return s
-
 end
 
 
 def starts_with_consonant? s
-  
+  if(s.size==0)
+    return false
+  end
   word = s[0]
   r = ['A','E','I','O','U','a','e','i','o','u']
-
   result = r.include?word
-  if(result == false &&((word > 'a' && word <= 'z')||(word > 'A' && word <= 'Z')))
-	  result = true
+  result = !result
+  if (word<'A'||word>'z'||(word<'a'&&word>'Z'))
+    result = !result
   end
-
   return result
-
-
 end
 
 def binary_multiple_of_4? s
   if s.size == 1 && s[0] != '0'
 	 return false
   end
-
   for i in 0...s.size
 	 if s[i].to_i != 0 && s[i].to_i != 1
 		return false
 	 end
   end
-
   if s[s.size-1].to_i == 0 && s[s.size-2].to_i == 0
 	return true
   end
-
   return false
-
 end
 
 # Part 3
